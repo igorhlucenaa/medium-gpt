97cb413b63e2bfdaf89b7be319c08c8b21210b96
Igor Lucena <igorhlucenaa@icloud.com>
2023-03-07 00:06:39 -0300

    'postado'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gpt-medium';
+  public pergunta!: string;
+  public resposta!: string;
+
+  constructor(private http: HttpClient) { }
+
+  fazerPergunta() {
+    const apiUrl = 'http://localhost:3000/completions';
+    this.http.post(apiUrl, { prompt: this.pergunta })
+      .subscribe((res: any) => {
+        this.resposta = res;
+      }, (err) => {
+        console.error(err);
+      });
+  }
 }
